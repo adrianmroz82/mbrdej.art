80b84da1ac9f4e92ef372ed631b4683e92d604b9
Adrian Mróz <hlepzje@gmail.com>
2023-02-28 18:50:56 +0100

    migrate from CRA to Vite // firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = getStorage(app);
+const db = getDatabase();
+// const projectFirestore = firebase.firestore();
+
+export { projectStorage, db };
