128185ee21b418f1bfb3b482ac9fa3d43d0bf41f
Adrian Mróz <hlepzje@gmail.com>
2023-03-02 22:55:25 +0100

    Navbar WIP

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import styles from "./Navbar.module.scss";
+import { NavLink } from "./components";
+import { isMobile } from "../../utils/utils";
+import logo from "./../../assets/images/logo.png";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [mobile, setIsMobile] = useState(false);
 
   const handleClick = () => {
     if (open) {
@@ -15,6 +19,22 @@ const Navbar = () => {
     setOpen(!open);
   };
 
+  const showButton = () => {
+    if (isMobile()) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  // i want to keep currently clicked link active
+
   const closeMobileMenu = () => {
     setOpen(false);
     document.body.style.overflow = "visible";
@@ -24,15 +44,48 @@ const Navbar = () => {
   const animateFrom = { opacity: 0, y: -40 };
   const animateTo = { opacity: 1, y: 40 };
 
+  const mobileMenu = (
+    <motion.div className={styles.navbarContainer}>
+      {open && (
+        <motion.div className={styles.navbarItems}>
+          <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.2 }}>
+            <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/">
+              Strona główna
+            </Link>
+          </motion.h2>
+          <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.4 }}>
+            <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/about">
+              O mnie
+            </Link>
+          </motion.h2>
+          <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.6 }}>
+            <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/contact">
+              Kontakt
+            </Link>
+          </motion.h2>
+          <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.8 }}>
+            <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/gallery">
+              Galeria
+            </Link>
+          </motion.h2>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.iconsContainer}>
-        <div className={styles.menuIcon} onClick={handleClick}>
-          <motion.i whileHover={{ scale: 1.3, rotate: 180 }} className={renderHamburger}></motion.i>
-        </div>
+        <motion.div className={styles.logoIcon}>
+          <img src={logo} alt="" width={"100%"} height="100vh" />
+        </motion.div>
 
-        <motion.div className={styles.mediaIcons}>
-          <a href="https://www.facebook.com/mbrdej.art" target="_blank" rel="noreferrer">
+        {mobile && (
+          <div className={styles.menuIcon} onClick={handleClick}>
+            <motion.i whileHover={{ scale: 1.3, rotate: 180 }} className={renderHamburger}></motion.i>
+          </div>
+        )}
+        {/* <a href="https://www.facebook.com/mbrdej.art" target="_blank" rel="noreferrer">
             <motion.i
               whileHover={{ scale: 1.3 }}
               style={{ marginRight: "20px", textDecoration: "none", color: "black" }}
@@ -49,35 +102,19 @@ const Navbar = () => {
               whileHover={{ scale: 1.3 }}
               style={{ marginRight: "20px", textDecoration: "none", color: "black" }}
               className="fa-brands fa-etsy"></motion.i>
-          </a>
-        </motion.div>
-      </div>
-      <motion.div className={styles.navbarContainer}>
-        {open && (
-          <motion.div className={styles.navbarItems}>
-            <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.2 }}>
-              <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/">
-                Strona główna
-              </Link>
-            </motion.h2>
-            <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.4 }}>
-              <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/about">
-                O mnie
-              </Link>
-            </motion.h2>
-            <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.6 }}>
-              <Link className={styles.navbarItem} onClick={closeMobileMenu} to="/contact">
-                Kontakt
-              </Link>
-            </motion.h2>
-            <motion.h2 initial={animateFrom} animate={animateTo} transition={{ delay: 0.8 }}>
-              <Link className={styles.navbarItem} onClick={closeMobileMenu} to="gallery">
-                Galeria
-              </Link>
-            </motion.h2>
+          </a> */}
+        {/* </motion.div> */}
+        {!mobile && (
+          <motion.div className={styles.navlinkContainer}>
+            <NavLink route="/about" name="O mnie" />
+            <NavLink route="/gallery" name="Galeria" />
+            {/* <NavLink route="/faq" name="FAQ" /> */}
+            <NavLink route="/orders" name="Zamówienia" />
+            <NavLink route="/contact" name="Kontakt" />
           </motion.div>
         )}
-      </motion.div>
+      </div>
+      {mobile && mobileMenu}
     </div>
   );
 };
