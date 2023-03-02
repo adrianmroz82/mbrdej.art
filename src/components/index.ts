822e3258259c3086f9205f51521b62c99dfbedc2
Adrian Mróz <hlepzje@gmail.com>
2023-03-02 23:01:56 +0100

    export MediaIcon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as AppHeader } from "./Navbar";
 export { default as LoadableImage } from "./LoadableImage";
+export { default as MediaIcon } from "./MediaIcon";
