8575a8ab1d29fc78ec41163da5d98f5c7705f22e
Adrian Mróz <hlepzje@gmail.com>
2023-03-04 22:12:22 +0100

    move navigation links to generic component

---
+++ src/components/Navbar/components/NavButton/NavButton.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { header } from "../../../../animations/animations";
+import styles from "./NavButton.module.scss";
+
+interface Props {
+  route: string;
+  name: string;
+}
+
+const NavButton = ({ route, name }: Props) => {
+  return (
+    <motion.h4 variants={header} whileHover={{ scale: 1.1 }}>
+      <Link className={styles.navLink} to={route}>
+        {name}
+      </Link>
+    </motion.h4>
+  );
+};
+
+export default NavButton;
