d9d3ebe1598c88e5b35568447ff3a67554dfbb5f
Adrian Mróz <hlepzje@gmail.com>
2023-03-04 22:10:43 +0100

    add initial OrdersPage

--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from "./OrdersPage.module.scss";
+import forest_orders from "./../../assets/images/forest_orders.jpg";
 
 const OrdersPage = () => {
   return (
-    <div>OrdersPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>
+        <img src={forest_orders} alt="image" />
+      </div>
+      <div className={styles.rightContainer}>
+        <h1>Zamówienie indywidualne</h1>
+        <p>
+          Chciałbyś mieć obraz o konkretnej tematyce, kolorach czy rozmiarze, według swojego upodobania? A może marzy ci
+          się uwiecznienie konkretnego kadru na płótnie? Chętnie stworzę obraz dla Ciebie, namalowany specjalnie do
+          Twojego wnętrza. Zapraszam do kontaktu. Razem stworzymy coś niepowtarzalnego.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default OrdersPage
+export default OrdersPage;
