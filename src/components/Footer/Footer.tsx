9b15d6585841af9d28a6646871e5b057b9835e32
Adrian Mróz <hlepzje@gmail.com>
2024-03-17 17:31:57 +0100

    remove unused css classes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,16 +6,14 @@ import classes from "./Footer.module.scss";
 export function Footer() {
   return (
     <motion.div className={classes.footerContainer}>
-      <div className={classes.footerInfo}>
-        <motion.div className={classes.mediaIcons}>
-          <MediaIcon className="fa-brands fa-facebook" href="https://www.facebook.com/mbrdej.art" />
-          <MediaIcon className="fa-brands fa-instagram" href="https://www.instagram.com/mbrdej.art" />
-          <MediaIcon
-            className="fa-brands fa-etsy"
-            href="https://www.etsy.com/pl/shop/mbrdejart?ref=seller-platform-mcnav"
-          />
-        </motion.div>
-      </div>
+      <motion.div className={classes.mediaIcons}>
+        <MediaIcon className="fa-brands fa-facebook" href="https://www.facebook.com/mbrdej.art" />
+        <MediaIcon className="fa-brands fa-instagram" href="https://www.instagram.com/mbrdej.art" />
+        <MediaIcon
+          className="fa-brands fa-etsy"
+          href="https://www.etsy.com/pl/shop/mbrdejart?ref=seller-platform-mcnav"
+        />
+      </motion.div>
     </motion.div>
   );
 }
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,7 @@
 import { motion } from "framer-motion";
 // import { SECTION1, SECTION2, SECTION3, SECTION4, SECTION5 } from "./content";
 
-import "./styles.scss";
-
-let easeing = [0.6, -0.05, 0.01, 0.99];
+const easeing = [0.6, -0.05, 0.01, 0.99];
 
 const fadeInUp = {
   initial: {
@@ -25,34 +23,27 @@ const fadeInUp = {
   },
 };
 
-const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
-
 export function AboutPage() {
   return (
     <motion.div initial="initial" animate="animate">
       <motion.div
-        className="content_wrapper"
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.3, ease: easeing }}
       >
-        <div className="left_content_wrapper">
-          <motion.p variants={fadeInUp}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, doloremque natus vero facere et,
-            exercitationem voluptatem quibusdam maiores quaerat est, nisi earum maxime quisquam dolores sint animi
-            libero ipsam quas!
-          </motion.p>
-        </div>
+        <motion.p variants={fadeInUp}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, doloremque natus vero facere et,
+          exercitationem voluptatem quibusdam maiores quaerat est, nisi earum maxime quisquam dolores sint animi libero
+          ipsam quas!
+        </motion.p>
 
-        <motion.div className="right_content_wrapper">
-          <motion.img
-            // src={process.env.PUBLIC_URL + "/images/bg.png"}
-            alt="bg"
-            initial={{ x: 200, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
-          />
-        </motion.div>
+        <motion.img
+          // src={process.env.PUBLIC_URL + "/images/bg.png"}
+          alt="bg"
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        />
       </motion.div>
     </motion.div>
   );
